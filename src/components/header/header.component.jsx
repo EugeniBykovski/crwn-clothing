027f98ef95faea4cd03aaa65e56646196239f2d2
EugeniBykovski <bykovskieugeni@mail.ru>
2021-04-22 20:29:37 +0300

    added card-dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 // importing SVG
 import { ReactComponent as Logo } from '../../assets/crown.svg'
@@ -16,7 +18,7 @@ const Header = ({ currentUser }) => (
     </Link>
     <div className="options">
       <Link className='option' to='/shop'>SHOP</Link>
-      <Link className='option' to='/shop'>CONTACT</Link>
+      <Link className='option' to='/contact'>CONTACT</Link>
 
       {
         currentUser ? 
@@ -24,7 +26,10 @@ const Header = ({ currentUser }) => (
         : 
         ( <Link className='option' to='/signin'>SIGN IN</Link> )
       }
+
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 )
 
