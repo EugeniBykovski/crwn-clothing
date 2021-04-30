2e5cc8189737e027b0c1a9bdebfa4921dd72afe8
EugeniBykovski <bykovskieugeni@mail.ru>
2021-04-30 23:34:13 +0300

    added collection component, redux optimization components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import { Route } from 'react-router-dom';
 
-// Data:
-import SHOP_DATA from './shop.data'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={ CollectionsOverview } />
+    <Route path={`${match.path}/:collectionId`} component={ CollectionPage } />
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={ id } { ...otherCollectionProps } />
-          ))
-        }
-      </div>
-    ) 
-  }
-}
-
-export default ShopPage
+export default ShopPage;
