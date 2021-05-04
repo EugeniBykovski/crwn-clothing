f1e5d494f698b99cedb3704f55ac698b8ed563f0
EugeniBykovski <bykovskieugeni@mail.ru>
2021-05-04 15:16:21 +0300

    added HOC pattern in to the app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 
 import { updateCollections } from '../../redux/shop/shop.actions'
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  }
+
   unsubscribeFromSnapshot = null
 
   componentDidMount() {
@@ -19,16 +28,19 @@ class ShopPage extends React.Component {
     collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap)
+
+      this.setState({ loading: false })
     })
   }
 
   render() {
     const { match } = this.props
+    const { loading } = this.state
 
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={ CollectionsOverview } />
-        <Route path={`${match.path}/:collectionId`} component={ CollectionPage } />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={ loading } { ...props } />} />
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={ loading } { ...props } />} />
       </div>
     );
   }
