cfd04aa926803a0865940cb8b46059f07bc27380
EugeniBykovski <bykovskieugeni@mail.ru>
2021-06-02 10:32:40 +0300

    added Profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
@@ -7,7 +7,18 @@ import { HomePageContainer } from './homepage.styles'
 // Func component:
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler
+      id='Directory'
+      onRender={(id, phase, actualDuration) => {
+        console.log({
+          id,
+          phase,
+          actualDuration
+        });
+      }}
+    >
+      <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
